09828a66eb76a7055565e44fa60988055c3c005a
raderro <95587591+raderro@users.noreply.github.com>
2023-11-09 22:25:07 +0100

    Category update

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const products = [
         id: 1,
         title: 'Amnesia Haze',
         price: 15.99,
+        category: 'haze',
         imageUrl: 'assets/amnesia-haze.png',
         description: 'Amnesia Haze osi�ga poziom THC rz�du 22%, przez co haj pojawia si� natychmiast i utrzymuje si� kilka godzin. Jej kwiaty zapewniaj� r�wnie� �redni poziom CBD, zapewniaj�c relaksuj�ce, niezamulaj�ce i terapeutyczne dzia�anie. Stymuluj�cy i umys�owy haj Amnesia Haze czyni j� �wietnym wyborem do palenia w ci�gu dnia.'
     },
@@ -10,6 +11,7 @@ const products = [
         id: 2,
         title: 'Purple Haze',
         price: 12.99,
+        category: 'haze',
         imageUrl: 'assets/purple-haze.png',
         description: 'Purple Haze to odmiana marihuany z dominacj� Sativy. Poziom THC oscyluje mi�dzy 18-23 %. Nasze nasiona Purple Haze to wynik skrzy�owania odmian Purple Kush i Haze. Nie wbije Ci� ona w fotel niczym Gorilla Glue, a wr�cz przeciwnie pobudzi do dzia�ania poprzez senny przyp�yw euforii.'
     },
@@ -17,6 +19,7 @@ const products = [
         id: 3,
         title: 'Lemon Haze',
         price: 9.99,
+        category: 'haze',
         imageUrl: 'assets/lemon-haze.png',
         description: 'Purple Haze to odmiana marihuany z dominacj� Sativy. Poziom THC oscyluje mi�dzy 18-23 %. Nasze nasiona Purple Haze to wynik skrzy�owania odmian Purple Kush i Haze. Nie wbije Ci� ona w fotel niczym Gorilla Glue, a wr�cz przeciwnie pobudzi do dzia�ania poprzez senny przyp�yw euforii.'
     },
@@ -24,6 +27,7 @@ const products = [
         id: 4,
         title: 'White Diesel Haze',
         price: 10.99,
+        category: 'haze',
         imageUrl: 'assets/diesel-haze.png',
         description: 'White Diesel Haze Automatic to szybka sativa w wersji kwitn�cej automatycznie. Ro�liny nie s� za wysokie i o wiele szybsze ni� tradycyjne sativy, przy tym zachowuj� dosy� dobr� produkcj� top�w. White wyst�puje w nazwie poniewa� topy s� mocno pokryte kryszta�kami �ywicy, podobnie jak w White Widow.'
     },
@@ -31,6 +35,7 @@ const products = [
         id: 5,
         title: 'White Widow',
         price: 8.99,
+        category: 'haze',
         imageUrl: 'assets/white-widow.png',
         description: 'White Widow obfituje w mircen, kt�ry jest dobrym �rodkiem przeciwb�lowym i rozlu�niaj�cym, a tak�e wykorzystywanym w terapii neuropatii i nudno�ci. Ma bardzo dobre dzia�anie przeciwb�lowe, ale dzi�ki silnej naturze Indica jest tak�e �wietnym kompanem w wypoczynku.'
     },
@@ -38,6 +43,7 @@ const products = [
         id: 6,
         title: 'MDMA - Niebieskie',
         price: 9.60,
+        category: 'mdma',
         imageUrl: 'assets/mdma_niebieska.jpg',
         description: 'Niebieska tabletka'
     },
@@ -45,6 +51,7 @@ const products = [
         id: 7,
         title: 'MDMA - Różowe',
         price: 9.60,
+        category: 'mdma',
         imageUrl: 'assets/mdma_rozowe.jpg',
         description: 'Rozowa tabletka'
     },
@@ -52,6 +59,7 @@ const products = [
         id: 8,
         title: 'MDMA - Żółte',
         price: 9.60,
+        category: 'mdma',
         imageUrl: 'assets/mdma_zolte.jpg',
         description: 'Żółta tabletka'
     },
@@ -59,6 +67,7 @@ const products = [
         id: 9,
         title: 'MDMA - Zielone',
         price: 9.60,
+        category: 'mdma',
         imageUrl: 'assets/mdma_zielone.jpg',
         description: 'Zielona tabletka'
     },
@@ -66,6 +75,7 @@ const products = [
         id: 10,
         title: 'MDMA - Losowe',
         price: 9.60,
+        category: 'mdma',
         imageUrl: 'assets/mdma_mix.jpg',
         description: 'Losowa tabletka'
     },
@@ -73,6 +83,7 @@ const products = [
         id: 11,
         title: 'Grzybki Halucynogenne',
         price: 11.50,
+        category: 'hallucinogens',
         imageUrl: 'assets/GrzybkiHalucynki.png',
         description: 'Grzybki Halucynki'
     },
@@ -80,6 +91,7 @@ const products = [
         id: 12,
         title: 'Haszysz - THC 62%',
         price: 21.99,
+        category: 'hash',
         imageUrl: 'assets/haszysz.jpg',
         description: 'Haszysz'
     },
@@ -87,6 +99,7 @@ const products = [
         id: 13,
         title: 'Haszysz - THC 48%',
         price: 19.99,
+        category: 'hash',
         imageUrl: 'assets/haszysz.jpg',
         description: 'Haszysz'
     },
@@ -94,6 +107,7 @@ const products = [
         id: 14,
         title: 'Haszysz - THC 57%',
         price: 20.99,
+        category: 'hash',
         imageUrl: 'assets/haszysz.jpg',
         description: 'Haszysz'
     },
@@ -101,6 +115,7 @@ const products = [
         id: 15,
         title: 'Haszysz - THC 69%',
         price: 22.50,
+        category: 'hash',
         imageUrl: 'assets/haszysz.jpg',
         description: 'Haszysz'
     },
@@ -143,7 +158,7 @@ const updateCartTotal = () => {
     cartTotal.textContent = `Suma: $${total.toFixed(2)}`;
 };
 
-products.forEach(product => {
+const renderProduct = (product) => {
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
     productCard.classList.add('fadeIn');
@@ -178,7 +193,7 @@ products.forEach(product => {
     productCard.appendChild(addToCartButton);
 
     productGrid.appendChild(productCard);
-});
+};
 
 const checkoutButton = document.getElementById("checkout-button");
 
@@ -189,3 +204,43 @@ checkoutButton.addEventListener("click", () => {
     localStorage.setItem("cartData", cartData);
     window.location.href = "platnosc.html";
 });
+
+const categorySelect = document.getElementById('category-select');
+const sortSelect = document.getElementById('sort-select');
+
+let currentCategory = 'all';
+let currentSort = 'priceAsc';
+
+// Funkcja do aktualizacji widoczności produktów na stronie
+const updateProductGrid = () => {
+    const selectedCategory = categorySelect.value;
+    const selectedSort = sortSelect.value;
+
+    // Filtruj produkty według kategorii
+    const filteredProducts = selectedCategory === 'all' ? products : products.filter(product => product.category === selectedCategory);
+
+    // Sortuj produkty
+    const sortedProducts = filteredProducts.sort((a, b) => {
+        if (selectedSort === 'priceAsc') {
+            return a.price - b.price;
+        } else if (selectedSort === 'priceDesc') {
+            return b.price - a.price;
+        }
+        // Dodaj dodatkowe przypadki sortowania, jeśli potrzebujesz
+    });
+
+    productGrid.innerHTML = ""; // Wyczyść istniejące produkty
+
+    sortedProducts.forEach(product => {
+        renderProduct(product);
+    });
+};
+
+// Obsługa zmiany wybranej kategorii
+categorySelect.addEventListener('change', updateProductGrid);
+
+// Obsługa zmiany wybranej opcji sortowania
+sortSelect.addEventListener('change', updateProductGrid);
+
+// Początkowe załadowanie produktów
+updateProductGrid();
